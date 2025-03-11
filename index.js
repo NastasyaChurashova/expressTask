e00302bfd050c38b31ef12fb51a35a62827b4e2a
Nastasya Churashova <gregarious@list.ru>
2025-03-11 01:50:23 +0100

    add: function loadViews

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const express = require("express");
+const fs = require("fs");
+const path = require("path");
 const app = express();
+const port = 3000;
 
-const countViews = {
-    "/": 0,
-    "/about":0
-}
+const viewsFile = path.join(__dirname, "views.json");
+
+const loadViews = () => {
+	try {
+		if (fs.existsSync(viewsFile)) {
+			const data = fs.readFileSync(viewsFile, "utf-8");
+			return JSON.parse(data);
+		}
+	} catch (error) {
+		console.error("Ошибка при сохранении данных просмотров:", error);
+	}
+};
+
+const countViews = loadViews();
 
 app.get("/", (req, res) => {
 	res.send("<h1>Это главная страница</h1>");
@@ -12,10 +25,6 @@ app.get("/", (req, res) => {
 app.get("/about", (req, res) => {
 	res.send("<h1>Это страница обо мне</h1>");
 });
-
-
-
-const port = 3000;
 
 app.listen(port, () => {
 	console.log(`Сервер запущен на порту ${port}`);
